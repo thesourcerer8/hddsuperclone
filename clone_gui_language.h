6e97c2ef5930dce7ad5ca50efe647bec6f0e140f
Philipp Gühring <pg@futureware.at>
2023-10-23 13:19:11 +0200

    Made constant parameters constant

--- clone_gui_language.h
+++ clone_gui_language.h
@@ -455,7 +455,7 @@ int translate_all_ccc(void);
 
 int translate_language_ccc(char *fromlang, char *translang, char *language, char *native);
 
-int translate_language_slow_ccc(char *fromlang, char *translang, char *language, char *native);
+int translate_language_slow_ccc(char *fromlang, char *translang, const char *language, const char *native);
 
 char* get_translated_data_ccc(char *url_data);
 
