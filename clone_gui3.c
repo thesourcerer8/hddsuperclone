5112298887b1c8e0fcc38617d44b5541b298aa08
Philipp Gühring <pg@futureware.at>
2026-08-20 01:21:01 +0200

    Removing const from function

--- clone_gui3.c
+++ clone_gui3.c
@@ -1084,7 +1084,7 @@ int translate_language_slow_ccc(char *fromlang, char *translang, const char *lan
 
     fprintf (stdout, "%d %s to %s  ", count, fromlang, translang);
     do_nanosleep_ccc(TRANSLATETIMERSLOW);  // this is a timer to deal with google translator
-    const char *data = get_translated_data_ccc(url_data);
+    char *data = get_translated_data_ccc(url_data);
     //fprintf (stdout, "%s\n", data);
     strcpy (return_data, data);
     free(data);
